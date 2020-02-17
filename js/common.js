9600c97019e86e63b75aab7bd08d227e5445a3b6
Anton Shaman <anton.shaman2015@gmail.com>
2020-02-17 21:14:04 +0200

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper('.swipper__card-carousel', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    speed: 300,
+    nav: true,
+    navigation: {
+        nextEl: '.btn-card-next',
+        prevEl: '.btn-card-prev',
+    },
+});
+
+
+var swiper = new Swiper('.investors-carousel', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    speed: 300,
+    nav: true,
+    navigation: {
+        nextEl: '.btn-investor-next',
+        prevEl: '.btn-investor-prev',
+    },
+});
